8455d9801b6e4c39fdcbd372edde012ff63148b9
ahmed <ahm.gmal@gmail.com>
2021-04-05 00:52:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+
+const driver = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = driver;
+    newDriver[key] = value;
+    return newDriver;
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = {...driver};
+    delete newDriver[key];
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    const newDriver = driver;
+    delete newDriver[key];
+    return newDriver;
+}
